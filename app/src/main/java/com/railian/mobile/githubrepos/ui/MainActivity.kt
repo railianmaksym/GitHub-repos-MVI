53107e615e16f1e54d159fa2584f59086b10b835
railian <railianmaksym@gmail.com>
2020-02-08 12:54:16 +0200

    init project

---
+++ app/src/main/java/com/railian/mobile/githubrepos/ui/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.railian.mobile.githubrepos.ui
+
+import androidx.fragment.app.FragmentActivity
+import com.railian.mobile.githubrepos.R
+
+class MainActivity : FragmentActivity(R.layout.main_activity) {
+
+}
